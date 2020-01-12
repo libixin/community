472cb4745c468395d2370f3697dd4428d43b5315
libixin <1490495067@qq.com>
2020-01-12 14:06:04 +0800

    Initial commit

---
+++ src/main/java/com/example/community/community/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.example.community.community.controller;
+
+import jdk.nashorn.internal.objects.annotations.Getter;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @Author: yoav
+ * @Data: 2020/1/12 13:41
+ * @Version 1.0
+ */
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
